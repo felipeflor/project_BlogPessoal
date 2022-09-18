c4211ffebf64aa59b2e51e1b8d9040318fe26cb9
Felipe <felipedossantosflor@gmail.com>
2022-09-18 18:36:43 -0300

    @FelipeFlor [Task: Component Login]

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './Components/estaticos/navbar/Navbar';
 import Footer from './Components/estaticos/footer/Footer';
 import Home from './paginas/home/Home';
+import Login from './paginas/login/Login';
 import './App.css';
 
 function App() {
@@ -11,7 +12,15 @@ function App() {
       <Router>
         <Navbar />
           <Switch>
-            <div>
+            <div style={{ minHeight: '100vh' }}>
+              <Route exact path='/'>
+                <Login />
+              </Route>
+
+              <Route path='/login'>
+                <Login />
+              </Route>
+
               <Route path='/home'>
                 <Home />
               </Route>
