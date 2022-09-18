3b73b41ac395bf8c8b75bfd1bd68051079047693
Felipe <felipedossantosflor@gmail.com>
2022-09-18 19:58:29 -0300

    @FelipeFlor [Task: Refactoring login and footer]

--- src/paginas/login/Login.tsx
+++ src/paginas/login/Login.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import {Grid, Box, Typography, TextField, Button} from '@material-ui/core';
-import {Link} from 'react-router-dom';
-import '.Login.css'
+import { Grid, Box, Typography, TextField, Button } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import './Login.css';
 
-function Login () {
+function Login() {
 
-    return(
+    return (
         <Grid container direction='row' justifyContent='center' alignItems='center'>
             <Grid alignItems='center' xs={6}>
                 <Box paddingX={20}>
                     <form>
-                        <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' style={{fontWeight: 'bold'}}>Entrar</Typography>
-                        <TextField id='usuario' label='usuario' variant='outlined' name='usuario' margin='normal' fullWidth />
-                        <TextField id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth />
+                        <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' className='textos1'>Entrar</Typography>
+                        <TextField id='usuario' label='usuário' variant='outlined' name='usuario' margin='normal' fullWidth />
+                        <TextField id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password'fullWidth />
                         <Box marginTop={2} textAlign='center'>
                             <Link to='/home' className='text-decorator-none'>
                                 <Button type='submit' variant='contained' color='primary'>
@@ -20,25 +20,19 @@ function Login () {
                                 </Button>
                             </Link>
                         </Box>
-
                     </form>
                     <Box display='flex' justifyContent='center' marginTop={2}>
                         <Box marginRight={1}>
                             <Typography variant='subtitle1' gutterBottom align='center'>Não tem uma conta?</Typography>
                         </Box>
-                        <Typography variant='subtitle2' gutterBottom align='center' style={{ fontWeight: 'bold'}}>Cadastre-se</Typography>
+                            <Typography variant='subtitle1' gutterBottom align='center' className='textos1'>Cadastre-se</Typography>
                     </Box>
                 </Box>
             </Grid>
-            <Grid xs={6} style={{
-                backgroundImage: `url(https://cdn.pixabay.com/photo/2019/10/02/10/37/cliff-of-moher-4520630__480.jpg)`,
-                backgroundRepeat: 'no-repeat', width: '100vh', minHeight: '100vh', backgroundSize: 'cover', backgroundPosition: 'center'
-            }}>
+            <Grid xs={6} className='imagem'>
 
             </Grid>
         </Grid>
-
-
     );
 }
 
